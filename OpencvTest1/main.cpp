8c85b6b986ca60044fdc82046bdb979bd761ac35
Kevin Yen <worm6206@gmail.com>
2014-07-03 15:03:14 +0800

    drum, not playable yet

--- OpencvTest1/main.cpp
+++ OpencvTest1/main.cpp
@@ -25,69 +25,9 @@ bool Showtext = false;
 vector<int> hull;
 vector<Point> points;
 string PlayingChordName;
+int drum;
+bool drums[10];
 
-string chord (Mat obj, int startX, int startY, int squaresizeX, int squaresizeY, int gap){
-    int xx = startX + 3*(squaresizeX + gap);
-    int yy = startY;
-    
-    for(int x = xx; x<xx + squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy + squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "C";
-        }
-    }
-    xx = xx - squaresizeX - gap;
-    for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Bm";
-        }
-    }
-    xx = xx - squaresizeX - gap;
-    for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Am";
-        }
-    }
-    xx = xx - squaresizeX - gap;
-    for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "G";
-        }
-    }
-    yy = yy + squaresizeY + gap;
-    xx = startX + 3*(squaresizeX + gap);
-    for(int x = xx; x<xx + squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy + squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "F";
-        }
-    }
-    xx = xx - squaresizeX - gap;
-    for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Em";
-        }
-    }
-    xx = xx - squaresizeX - gap;
-    for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Dm";
-        }
-    }
-    xx = xx - squaresizeX - gap;
-    for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
-        for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
-            Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "C";
-        }
-    }
-    return "N";
-}
 
 void SendChord(String chord){
     if (chord == "C") ChordMajor(60);
@@ -101,12 +41,12 @@ void SendChord(String chord){
 
 
 void RD (Mat obj, cv::Point a,cv::Point b){
-    for(int x = a.x; x<b.x; x+=3){
-        for(int y = a.y; y<b.y; y+=3){
+    for(int x = a.x; x<b.x; x+=1){
+        for(int y = a.y; y<b.y; y+=1){
             Scalar colour = obj.at<uchar>(cv::Point(x,y));
             if(colour.val[0]!=0){
                 if (!picked){
-                   SendChord(chord(obj, 830, 250, 70, 70, 30));
+                    cout << "HO" << endl;
                 }
                 picked = true;
                 return;
@@ -118,25 +58,27 @@ void RD (Mat obj, cv::Point a,cv::Point b){
 }
 
 void hmm(Mat obj){
-    rectangle(obj, Point(500,320), Point(600,360), Scalar(255,0,0));
-    rectangle(obj, Point(1200,250), Point(1130,320), Scalar(255,0,0));
-    rectangle(obj, Point(1100,250), Point(1030,320), Scalar(255,0,0));
-    rectangle(obj, Point(1000,250), Point( 930,320), Scalar(255,0,0));
-    rectangle(obj, Point( 900,250), Point( 830,320), Scalar(255,0,0));
-    rectangle(obj, Point(1200,360), Point(1130,430), Scalar(255,0,0));
-    rectangle(obj, Point(1100,360), Point(1030,430), Scalar(255,0,0));
-    rectangle(obj, Point(1000,360), Point( 930,430), Scalar(255,0,0));
-    rectangle(obj, Point( 900,360), Point( 830,430), Scalar(255,0,0));
+    //    rectangle(obj, Point(500,320), Point(600,360), Scalar(255,0,0));
     flip(obj  , obj, 1);
-    putText(CF1, "C", Point(80,320), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "Bm", Point(180,320), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "Am", Point(280,320), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "G", Point(380,320), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "F", Point(80,420), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "Em", Point(180,420), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "Dm", Point(280,420), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "C", Point(380,420), 1, 3, Scalar(0,0,255),2);
-    putText(CF1, "PICK", Point(690,350), 1, 2, Scalar(0,0,255),3);
+    rectangle(obj, Point(100,120), Point(120,320), Scalar(255,0,0));
+    rectangle(obj, Point(150,50), Point(350,70), Scalar(255,0,0));
+    rectangle(obj, Point(100,370), Point( 120,570), Scalar(255,0,0));
+//    rectangle(obj, Point( 1000,150), Point( 1200,170), Scalar(255,0,0));
+    
+//    rectangle(obj, Point(100,550), Point(300,570), Scalar(255,0,0));
+//    rectangle(obj, Point(400,550), Point(600,570), Scalar(255,0,0));
+//    rectangle(obj, Point(700,550), Point( 900,570), Scalar(255,0,0));
+    rectangle(obj, Point( 1000,550), Point( 1200,570), Scalar(255,0,0));
+    //flip(obj  , obj, 1);
+//    putText(CF1, "C", Point(80,320), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "Bm", Point(180,320), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "Am", Point(280,320), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "G", Point(380,320), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "F", Point(80,420), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "Em", Point(180,420), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "Dm", Point(280,420), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "C", Point(380,420), 1, 3, Scalar(0,0,255),2);
+//    putText(CF1, "PICK", Point(690,350), 1, 2, Scalar(0,0,255),3);
 
     imshow("cam", obj);
 }
@@ -153,7 +95,11 @@ int main() {
         cvtColor(diff2,diff2,CV_RGB2GRAY);
         threshold(diff2, diff2, ThreshLevel, 255, THRESH_BINARY);
         
-        RD(diff2, Point(500,320), Point(600,340));
+//        RD(diff2, Point(500,320), Point(600,340));
+        
+        flip(diff2  , diff2, 1);
+        RD(diff2, Point( 1000,550), Point( 1200,570));
+        cout << PlayingChordName << endl;
         hmm(CF1);
         
         inputkey = waitKey(1);
