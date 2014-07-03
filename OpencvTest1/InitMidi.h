6186122ead2b55d333a526fbe2ddcf14a5f3a3d4
Kevin Yen <worm6206@gmail.com>
2014-07-03 15:49:11 +0800

    improved the pick part

--- OpencvTest1/InitMidi.h
+++ OpencvTest1/InitMidi.h
@@ -15,72 +15,21 @@ std::vector<unsigned char> midiMessage;
 std::vector<unsigned char> message;
 RtMidiOut *midiout = new RtMidiOut();
 
-void ChordMajor(int a){
+void Chord(int a){
     midiMessage.push_back( 144 );
     midiMessage.push_back( a );
     midiMessage.push_back( 90 );
     midiout->sendMessage( &midiMessage );
-    usleep(50000);
     midiMessage.clear();
-    midiMessage.push_back( 144 );
-    midiMessage.push_back( a+4 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    usleep(50000);
-    midiMessage.clear();
-    midiMessage.push_back( 144 );
-    midiMessage.push_back( a+7 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    usleep(50000);
+    }
+void clear(){
+    int t =12;
+    while(t<25){
     midiMessage.push_back( 128 );
-    midiMessage.push_back( a );
+    midiMessage.push_back( t );
     midiMessage.push_back( 90 );
     midiout->sendMessage( &midiMessage );
     midiMessage.clear();
-    midiMessage.push_back( 128 );
-    midiMessage.push_back( a+4 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    midiMessage.clear();
-    midiMessage.push_back( 128 );
-    midiMessage.push_back( a+7 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
+    t++;
+    }
 }
-
-void ChordMinor(int a){
-    midiMessage.push_back( 144 );
-    midiMessage.push_back( a );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    usleep(50000);
-    midiMessage.clear();
-    midiMessage.push_back( 144 );
-    midiMessage.push_back( a+3 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    usleep(50000);
-    midiMessage.clear();
-    midiMessage.push_back( 144 );
-    midiMessage.push_back( a+7 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    usleep(50000);
-    midiMessage.push_back( 128 );
-    midiMessage.push_back( a );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    midiMessage.clear();
-    midiMessage.push_back( 128 );
-    midiMessage.push_back( a+3 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-    midiMessage.clear();
-    midiMessage.push_back( 128 );
-    midiMessage.push_back( a+7 );
-    midiMessage.push_back( 90 );
-    midiout->sendMessage( &midiMessage );
-}
-
-
--- OpencvTest1/main.cpp
+++ OpencvTest1/main.cpp
@@ -19,12 +19,13 @@ Mat CF2;
 int ThreshLevel=20;
 int hullcount;
 int inputkey;
+int judge;
 bool picked = 0;
 Scalar rec1;
 bool Showtext = false;
 vector<int> hull;
 vector<Point> points;
-string PlayingChordName;
+string PlayingChordName,PlayedChordName;
 
 string chord (Mat obj, int startX, int startY, int squaresizeX, int squaresizeY, int gap){
     int xx = startX + 3*(squaresizeX + gap);
@@ -33,21 +34,21 @@ string chord (Mat obj, int startX, int startY, int squaresizeX, int squaresizeY,
     for(int x = xx; x<xx + squaresizeX; x = x + squaresizeX/4){
         for(int y = yy; y<yy + squaresizeY; y = y + squaresizeY/4){
             Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "C";
+            if(colour.val[0]!=0) return "CC";
         }
     }
     xx = xx - squaresizeX - gap;
     for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
         for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
             Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Bm";
+            if(colour.val[0]!=0) return "B";
         }
     }
     xx = xx - squaresizeX - gap;
     for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
         for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
             Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Am";
+            if(colour.val[0]!=0) return "A";
         }
     }
     xx = xx - squaresizeX - gap;
@@ -69,14 +70,14 @@ string chord (Mat obj, int startX, int startY, int squaresizeX, int squaresizeY,
     for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
         for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
             Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Em";
+            if(colour.val[0]!=0) return "E";
         }
     }
     xx = xx - squaresizeX - gap;
     for(int x = xx; x<xx+squaresizeX; x = x + squaresizeX/4){
         for(int y = yy; y<yy+squaresizeY; y = y + squaresizeY/4){
             Scalar colour = obj.at<uchar>(Point(x,y));
-            if(colour.val[0]!=0) return "Dm";
+            if(colour.val[0]!=0) return "D";
         }
     }
     xx = xx - squaresizeX - gap;
@@ -90,31 +91,35 @@ string chord (Mat obj, int startX, int startY, int squaresizeX, int squaresizeY,
 }
 
 void SendChord(String chord){
-    if (chord == "C") ChordMajor(60);
-    else if (chord == "Dm") ChordMinor(62);
-    else if (chord == "Em") ChordMinor(64);
-    else if (chord == "Am") ChordMinor(69);
-    else if (chord == "Bm") ChordMinor(71);
-    else if (chord == "F") ChordMajor(65);
-    else if (chord == "G") ChordMajor(67);
+    if (chord == "C") Chord(24);
+    else if (chord == "D") Chord(14);
+    else if (chord == "E") Chord(16);
+    else if (chord == "A") Chord(21);
+    else if (chord == "B") Chord(23);
+    else if (chord == "F") Chord(17);
+    else if (chord == "G") Chord(19);
+    else if (chord == "CC") Chord(12);
 }
 
 
 void RD (Mat obj, cv::Point a,cv::Point b){
+    judge =0;
     for(int x = a.x; x<b.x; x+=3){
         for(int y = a.y; y<b.y; y+=3){
             Scalar colour = obj.at<uchar>(cv::Point(x,y));
-            if(colour.val[0]!=0){
-                if (!picked){
-                   SendChord(chord(obj, 830, 250, 70, 70, 30));
-                }
-                picked = true;
-                return;
-            }
-            picked= false;
+            judge+=colour.val[0];
+            
         }
     }
-    return;
+    if(judge!=0){
+        PlayingChordName = chord(obj, 830, 250, 70, 70, 30);
+        if (PlayingChordName == PlayedChordName)return;
+        clear();
+        SendChord(PlayingChordName);
+        PlayedChordName = PlayingChordName;
+        return;
+    }
+    PlayedChordName = "";
 }
 
 void hmm(Mat obj){
@@ -145,7 +150,6 @@ int main() {
     stream2.read(CF2);
     midiout->openPort( 0 );
     while (true) {
-        Showtext = false;
         stream1.read(CF1);
         Mat diff2 = CF2 - CF1;
         Mat diff = CF1 - CF2;
@@ -154,13 +158,14 @@ int main() {
         threshold(diff2, diff2, ThreshLevel, 255, THRESH_BINARY);
         
         RD(diff2, Point(500,320), Point(600,340));
-        hmm(CF1);
+        hmm(diff2);
         
         inputkey = waitKey(1);
         switch (inputkey) {
             case 27:
                 stream1.release();
                 stream2.release();
+                clear();
                 return 0;
                 break;
             case 63232://up
